97a7c9f154a8213313ce5aa297ae615d0deba159
tmasuyama1114 <tmasuyama1114@gmail.com>
2020-08-03 19:34:47 +0900

    This is first commit.

---
+++ diary/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'diary'
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),  # /diary
+    path('add/', views.AddView.as_view(), name='add'),  # /diary/add
+    path('update/<int:pk>/', views.UpdateView.as_view(), name='update'),  # diary/update/1
+    path('delete/<int:pk>/', views.DeleteView.as_view(), name='delete'),  # /diary/delete/1
+    path('detail/<int:pk>/', views.DetailView.as_view(), name='detail'),  # /diary/detail/1
+]
---
+++ diary/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import DayCreateForm
+from .models import Day
+
+
+class IndexView(generic.ListView):
+    model = Day
+    paginate_by = 3
+
+
+class AddView(generic.CreateView):
+    model = Day
+    form_class = DayCreateForm
+    success_url = reverse_lazy('diary:index')  # /diary/
+
+
+class UpdateView(generic.UpdateView):
+    model = Day
+    form_class = DayCreateForm
+    success_url = reverse_lazy('diary:index')
+
+
+class DeleteView(generic.DeleteView):
+    model = Day
+    success_url = reverse_lazy('diary:index')
+
+
+class DetailView(generic.DetailView):
+    model = Day
+
+
